27bf22318eb497ec83ab10483b51f9f5c58605a9
Kevin Dunne <kevinmdunne@users.noreply.github.com>
2024-06-08 22:47:43 +0100

    initial commit

---
+++ src/test/java/com/thesharegame/portfolioservice/PortfolioServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thesharegame.portfolioservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
