859bf3ee5432c03299d39221a21e001cf1e54822
Kevin Dunne <kevinmdunne@users.noreply.github.com>
2024-10-04 23:29:51 +0100

    add external properties files

--- src/main/java/com/thesharegame/portfolioservice/controller/PortfolioController.java
+++ src/main/java/com/thesharegame/portfolioservice/controller/PortfolioController.java
@@ -2,10 +2,12 @@ package com.thesharegame.portfolioservice.controller;
 
 import com.thesharegame.portfolioservice.dto.PortfolioDto;
 import com.thesharegame.portfolioservice.service.PortfolioService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("portfolio")
+@Slf4j
 public class PortfolioController {
 
     private PortfolioService portfolioService;
@@ -16,21 +18,25 @@ public class PortfolioController {
 
     @GetMapping("/{id}")
     public PortfolioDto getPortfolioById(@PathVariable String id){
+        log.info("Calling getPortfolioById");
         return this.portfolioService.getPortfolioById(id);
     }
 
     @DeleteMapping("/{id}")
     public void deletePortfolio(@PathVariable String id){
+        log.info("Calling deletePortfolio");
         this.portfolioService.deletePortfolio(id);
     }
 
     @PostMapping("/")
     public PortfolioDto savePortfolio(@RequestBody PortfolioDto portfolioDto){
+        log.info("Calling savePortfolio");
         return this.portfolioService.savePortfolio(portfolioDto);
     }
 
     @GetMapping("/addDummyData")
     public PortfolioDto addDummyData(){
+        log.info("Calling addDummyData");
         return this.portfolioService.addDummyData();
     }
 }
