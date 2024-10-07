c10b7b2cb6fae5cb0cea3497f04a35e3adaec128
Kevin Dunne <kevinmdunne@users.noreply.github.com>
2024-10-07 23:23:40 +0100

    enable eureka

--- src/main/java/com/thesharegame/portfolioservice/PortfolioServiceApplication.java
+++ src/main/java/com/thesharegame/portfolioservice/PortfolioServiceApplication.java
@@ -1,10 +1,17 @@
 package com.thesharegame.portfolioservice;
 
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Lazy;
 
 @SpringBootApplication
 public class PortfolioServiceApplication {
+
+	@Autowired
+	@Lazy
+	private EurekaClient eurekaClient;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PortfolioServiceApplication.class, args);
